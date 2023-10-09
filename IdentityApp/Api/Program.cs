3ceae9d053f3919040cf1322e5933c711f3ca560
Alghirdas Nasti <54104013+AlecNasti@users.noreply.github.com>
2023-10-10 00:39:51 +0200

    Added Net Identity

--- IdentityApp/Api/Program.cs
+++ IdentityApp/Api/Program.cs
@@ -1,3 +1,12 @@
+using Api.Data;
+using Api.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,44 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<JWTService>();
+
+builder.Services.AddIdentityCore<User>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+
+    options.SignIn.RequireConfirmedEmail = true;
+
+})
+    .AddRoles<IdentityRole>() //aggiungi ruoli
+    .AddRoleManager<RoleManager<IdentityRole>>() //usa il manager per creare ruoli
+    .AddEntityFrameworkStores<Context>() // aggiungere il contesto
+    .AddSignInManager<SignInManager<User>>() // usare il manager per il log in
+    .AddUserManager<UserManager<User>>() // usare il manager per creare utenti
+    .AddDefaultTokenProviders(); // creare token per la conferma dell'email
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+            ValidIssuer = builder.Configuration["JWT:issuer"],
+            ValidateIssuer = true,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -18,6 +65,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
